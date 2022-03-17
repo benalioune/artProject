32615815c7a8cf7409faa34845fdb607f67ceae4
diallo <benalioune6@gmail.com>
2022-03-17 17:38:57 +0100

    adding add-to-cart modal

--- src/app/shop/add-to-cart-modal/add-to-cart-modal.component.ts
+++ src/app/shop/add-to-cart-modal/add-to-cart-modal.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Products } from 'src/app/model/products';
+import { environment } from 'src/environments/environment';
+
 
 @Component({
   selector: 'app-add-to-cart-modal',
@@ -6,6 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-to-cart-modal.component.css']
 })
 export class AddToCartModalComponent implements OnInit {
+
+
+  @Input() products: Products[]; // ce component n'existerait pas si on ne clique pas sur un produit | le @input permettra de recuperer l'id du produit cliqué et generer le template view product du produit cliqué
+
+  urlImagePrefix = `${environment.urlImagePrefix}`;
+
 
   constructor() { }
 
